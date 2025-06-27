a3c81d7587917607f0aa383bd3e2cd9b53b85f2f
Rakesh Penugonda <119618769+Rakesh-116@users.noreply.github.com>
2025-06-27 10:06:06 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="">
+      <h1 className="text-5xl text-gray-700">Welcome to the React App</h1>
+      <p className="text-gray-500 mt-4">
+        This is a simple React application using Tailwind CSS for styling.
+      </p>
+      <p className="text-gray-500 mt-2">
+        You can start building your components and styles here.
+      </p>
+    </div>
+  );
 };
 
 export default App;
